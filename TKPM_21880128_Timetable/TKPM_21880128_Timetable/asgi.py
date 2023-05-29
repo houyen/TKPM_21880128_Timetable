1752cfdf5dc4a06b063105784d29ffad3068aec0
houyen <34957287+houyen@users.noreply.github.com>
2023-05-29 21:27:39 +0700

    [ADD] create project

---
+++ TKPM_21880128_Timetable/TKPM_21880128_Timetable/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for TKPM_21880128_Timetable project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TKPM_21880128_Timetable.settings')
+
+application = get_asgi_application()
