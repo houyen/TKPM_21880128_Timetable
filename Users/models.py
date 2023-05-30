b2ee723e0014237e01c4401c9432a27abfb0c605
houyen <34957287+houyen@users.noreply.github.com>
2023-05-31 00:41:29 +0700

    [ADD] Add required depends

--- Users/models.py
+++ Users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext_lazy as _
 from schools.models import School
 from users.managers import CustomUserManager
 
